9cd06ed44ffdc367b443fa1d69dc449884829389
estevanbs <estevanbartmann@gmail.com>
2026-03-05 22:51:38 -0300

    fix prisma migration to v7

--- libs/database/src/lib/prisma.service.ts
+++ libs/database/src/lib/prisma.service.ts
@@ -1,11 +1,23 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma-client/client.js';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is required to initialize PrismaService');
+    }
+
+    const adapter = new PrismaPg({ connectionString });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
